438efc12e6e6e80b9e5b830337ab251571990a93
brianh014 <brianh014@gmail.com>
2013-11-11 00:34:39 -0600

    Actually fixed link format and added recommender

--- getWiki.py
+++ getWiki.py
@@ -17,21 +17,21 @@ class Wiki(object):
             results = wikipedia.page(term)
         except wikipedia.exceptions.DisambiguationError as e:
             if len(e.options) >= 6:
-                response = response + "Your query to WikiBot returned a disambiguation page. Here are the top 5 pages:  \n"
+                response = response + "Your query " + term + " to WikiBot returned a disambiguation page. Here are the top 5 pages:  \n"
             else:
-                response = response + "Your query to WikiBot returned a disambiguation page. Here are the pages:  \n"
+                response = response + "Your query " + term + " to WikiBot returned a disambiguation page. Here are the pages:  \n"
             count = 0
             
             for option in e.options:
                 count += 1
                 if count >= 6:
-                    break
+                    break  
                 try:
                     x = wikipedia.page(option.encode('ascii', 'ignore'))
                 except wikipedia.exceptions.PageError as t:
-                    response = response + x.title +"  \n"
+                    response = response + option + ": Sorry couldn't fetch the link.  \n"
                 except wikipedia.exceptions.DisambiguationError as t:
-                    response = response + x.title +"  \n"
+                    response = response + option + ": Sorry couldn't fetch the link.  \n"
                 else:
                     response = response + "[" + x.title + "](" + self.formaturl(x.url) + "): " + wikipedia.summary(option.encode('ascii', 'ignore'), sentences=1) + "  \n"
                     
@@ -43,14 +43,28 @@ class Wiki(object):
             return response
         
         summary = (results.summary[:9000] + '..') if len(results.summary) > 9000 else results.summary
-        response = response + "Here is what WikiBot found on \"" + term + "\":\n\n" + results.title +"  \n" + summary + "\n\n" + "Link to article [" + results.title + "](" + self.formaturl(results.url) + ")"
+        response = response + "Here is what WikiBot found on \"" + term + "\":\n\n" + results.title +"  \n" + summary + "\n\n" + "Link to article [" + results.title + "](" + self.formaturl(results.url) + ")\n\n"
+        
+        response = self.recommender(results, response, term)
         
         response = self.wikifooter(response)
+        
+        return response
+    
+    def recommender(self, page, response, term):
+        recs = wikipedia.search(term,results=4)
+        if len(recs) > 1:
+            response = response + "Here are other related articles:  \n"
+            for rec in recs:
+                if rec != page.title:
+                    x = wikipedia.page(rec)
+                    response = response + "[" + x.title + "](" + self.formaturl(x.url) + ")  \n"
+
         return response
     
     def formaturl(self, url):
-        url.replace("(","\(")
-        url.replace(")","\)")
+        url = url.replace("(","\(")
+        url = url.replace(")","\)")
         return url
     
     def wikifooter(self, input):
@@ -59,5 +73,5 @@ class Wiki(object):
         
 if __name__ == "__main__":
     search = Wiki()
-    print search.searchwiki("VII Corps", "fr")
+    print search.searchwiki("Texas A&M", "en")
                 
