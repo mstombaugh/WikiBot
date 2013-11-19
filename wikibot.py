3e9b513e3efed3cd2024e95a259d4ebe3e0c8485
Michael Stombaugh <mstombaugh@tamu.edu>
2013-11-18 23:12:57 -0600

    moved things into their own functions

--- wikibot.py
+++ wikibot.py
@@ -2,32 +2,112 @@ import praw
 import json
 from getWiki import Wiki
 
-def main():
-    stats = {}
-    with open('wikibot_info.json') as cfgFile:
-        for line in cfgFile:
-            config = json.loads(line.strip())
-            break
-    with open('already_done') as f:
-        for line in f:
-            already_done=json.loads(line.strip())
-            break
-    with open('stats') as statistics:
-        for line in statistics:
+class WikiBot:
+    def setUp(self):
+        self.stats = {}
+        self.already_done = {}
+        self.config = {}
+        with open('wikibot_info.json') as cfgFile:
+            for line in cfgFile:
+                self.config = json.loads(line.strip())
+                
+        with open('already_done') as f:
+            for line in f:
+                self.already_done=json.loads(line.strip())
+                
+        with open('stats') as statistics:
+            for line in statistics:
+                try:
+                    self.stats = json.loads(line.strip())
+                    
+                except Exception as e:
+                    self.stats = json.loads('{"count": 1, "subreddits": {}, "categories": {}, "queries": []}'.strip())
+                    print e
+        self.r=praw.Reddit(user_agent = 'Wiki Bot')
+        self.r.login(self.config['user'],self.config['pass'])
+        self.wikibot_names = ['wikibot!', 'wiki-bot!', '!wikibot']
+        self.wiki = Wiki()
+
+    def run(self):
+        try:
+            for comment in praw.helpers.comment_stream(self.r,'all', limit = None):
+                op_text = comment.body.lower()
+                self.parseComments(op_text, subreddit=comment.subreddit.display_name , onReddit = True, comment = comment)
+        except KeyboardInterrupt:
+            with open('already_done','w+') as f:
+                f.write(json.dumps(self.already_done)+'\n')
+            with open('stats','w+') as statistics:
+                statistics.write(json.dumps(self.stats)+'\n')
+            print('Goodbye!')
+        except Exception as e:
+            with open('already_done','w+') as f:
+                f.write(json.dumps(self.already_done)+'\n')
+            with open('stats','w+') as statistics:
+                statistics.write(json.dumps(self.stats)+'\n')
+            print 'error2 ' + str(e)
+            self.run()
+        
+    def convertLanguageCode(self,language):
             try:
-                stats = json.loads(line.strip())
-                break
-            except Exception as e:
-                print e
-    r=praw.Reddit(user_agent = 'Wiki Bot')
-    r.login(config['user'],config['pass'])
-    wikibot_names = ['wikibot!', 'wiki-bot!', '!wikibot']
-    wiki = Wiki()
-    try:
-        for comment in praw.helpers.comment_stream(r,'all', limit = None):
-            op_text = comment.body.lower()
-            calls_wikibot = any(string in op_text for string in wikibot_names)
-            if comment.fullname not in already_done['already_done'] and calls_wikibot:
+                return{
+                    'english'   : 'en',
+                    'dutch'     : 'nl',
+                    'german'    : 'de',
+                    'sweedish'  : 'sv',
+                    'french'    : 'fr',
+                    'italian'   : 'it',
+                    'russian'   : 'ru',
+                    'spanish'   : 'es',
+                    'polish'    : 'pl',
+                    'waray-waray' : 'war',
+                    'cebuano'   : 'ceb',
+                    'cietnamese': 'vi',
+                    'japanese'  : 'ja',
+                    'portuguese': 'pt',
+                    'chinese'   : 'zh',
+                    'ukranian'  : 'uk',
+                    'catalan'   : 'ca',
+                    'norwegian' : 'no',
+                    'finnish'   : 'fi',
+                    'persian'   : 'fa',
+                    'indonesian': 'id',
+                    'czech'     : 'cs',
+                    'korean'    : 'ko',
+                    'hungarian' : 'hu',
+                    'arabic'    : 'ar',
+                    'malay'     : 'ms',
+                    'romanian'  : 'ro',
+                    'serbian'   : 'sr',
+                    'minangkabau':'min',
+                    'turkish'   : 'tr',
+                    'kazakh'    : 'kk',
+                    'slovak'    : 'sk',
+                    'esperanto' : 'eo',
+                    'danish'    : 'da',
+                    'basque'    : 'eu',
+                    'lithuanian': 'lt',
+                    'bulgarian' : 'bg',
+                    'hebrew'    : 'he',
+                    'croatian'  : 'hr',
+                    'slovenian' : 'sl',
+                    'uzbek'     : 'uz',
+                    'volapuk'   : 'vo',
+                    'estonian'  : 'et',
+                    'hindi'     : 'hi',
+                    'galician'  : 'gl',
+                    'nynorsk'   : 'nn',
+                    'simple'    : 'simple'
+                }[language.lower()]
+            except:
+                return language
+                
+    def parseComments(self,op_text='', subreddit='missingSubreddit', onReddit = False, comment = None):
+            if onReddit:
+                fullname = comment.fullname
+            else:
+                fullname = ''
+            calls_wikibot = any(string in op_text for string in self.wikibot_names)
+            if fullname not in self.already_done['already_done'] and calls_wikibot or not onReddit:
                 codeWordFound = firstQuotes = secondQuotes = False
                 wikiError=False
                 wikiRequest=[]
@@ -62,133 +142,66 @@ def main():
                     msg = msg.replace('&lt;',"<")
                     msg = msg.strip()
                     print msg + ' ' + language
-                    wikiArticle, categories = wiki.searchwiki(msg,convertLanguageCode(language),False)
+                    wikiArticle, categories = self.wiki.searchwiki(msg,self.convertLanguageCode(language),False)
                
                 #print comment.subreddit.display_name
-                comment.reply(wikiArticle)
-                already_done['already_done'].append(comment.fullname)
+                if(onReddit):
+                    comment.reply(wikiArticle)
+                    self.already_done['already_done'].append(fullname)
                 
                 #stats section
-                if not stats['categories']:
-                    stats['categories'] = {}
-                if not stats['subreddits']:
-                    stats['subreddits'] = {}
-                if not stats['queries']:
-                    stats['queries'] = []
-                if not stats['count']:
-                    stats['count'] = 0
+                if not self.stats['categories']:
+                    self.stats['categories'] = {}
+                if not self.stats['subreddits']:
+                    self.stats['subreddits'] = {}
+                if not self.stats['queries']:
+                    self.stats['queries'] = []
+                if not self.stats['count']:
+                    self.stats['count'] = 0
                 #print 'loaded base dict'
                 #make sure category is in the list
-                if comment.subreddit.display_name not in stats['categories']:
-                    stats['categories'][comment.subreddit.display_name] = {}
-                if comment.subreddit.display_name not in stats['subreddits']:
-                    stats['subreddits'][comment.subreddit.display_name] = {}
+                if subreddit not in self.stats['categories']:
+                    self.stats['categories'][subreddit] = {}
+                if subreddit not in self.stats['subreddits']:
+                    self.stats['subreddits'][subreddit] = {}
                 #print stats
                 #add categories to subreddit
                 for cat in categories:
                     try:
-                        if cat not in stats['categories'][comment.subreddit.display_name]:
-                            stats['categories'][comment.subreddit.display_name][cat] = 1
+                        if cat not in self.stats['categories'][subreddit]:
+                            self.stats['categories'][subreddit][cat] = 1
                         else:
-                             stats['categories'][comment.subreddit.display_name][cat] += 1
+                             self.stats['categories'][subreddit][cat] += 1
                     except:
-                        stats['categories'][comment.subreddit.display_name][cat] = 1
+                        self.stats['categories'][subreddit][cat] = 1
                         print 'exception'
                 #total count
                 try:
-                    stats['count'] += 1
+                    self.stats['count'] += 1
                 except:
-                    stats['count'] = 1
+                    self.stats['count'] = 1
                 #individual subreddit count    
                 try:
-                    stats['subreddits'][comment.subreddit.display_name]['count'] += 1
+                    self.stats['subreddits'][subreddit]['count'] += 1
                 except:
-                    if not stats['subreddits'][comment.subreddit.display_name]:
-                        stats['subreddits'][comment.subreddit.display_name] = {}
-                    stats['subreddits'][comment.subreddit.display_name]['count'] = 1
+                    if not self.stats['subreddits'][subreddit]:
+                        self.stats['subreddits'][subreddit] = {}
+                    self.stats['subreddits'][subreddit]['count'] = 1
                 #last 10 queries
                 try:
-                    stats['queries'].insert(0,msg)
-                    if len(stats['queries']) >= 10:
-                        stats['queries'].pop()
+                    self.stats['queries'].insert(0,msg)
+                    if len(self.stats['queries']) > 10:
+                        self.stats['queries'].pop()
                 except Exception as e:
-                    if not stats['queries']:
-                        stats['queries'] = []
-                    stats['queries']= [msg]
+                    if not self.stats['queries']:
+                        self.stats['queries'] = []
+                    self.stats['queries']= [msg]
                     print 'creating queries list' + str(e)
-                #save to JSON file
                 with open('stats','w+') as statistics:
-                    statistics.write(json.dumps(stats)+'\n')
-    except KeyboardInterrupt:
-        with open('already_done','w+') as f:
-            f.write(json.dumps(already_done)+'\n')
-        with open('stats','w+') as statistics:
-            statistics.write(json.dumps(stats)+'\n')
-        print('Goodbye!')
-    except Exception as e:
-        with open('already_done','w+') as f:
-            f.write(json.dumps(already_done)+'\n')
-        with open('stats','w+') as statistics:
-            statistics.write(json.dumps(stats)+'\n')
-        print 'error2 ' + str(e)
-        main()
-    
-def convertLanguageCode(language):
-        try:
-            return{
-                'english'   : 'en',
-                'dutch'     : 'nl',
-                'german'    : 'de',
-                'sweedish'  : 'sv',
-                'french'    : 'fr',
-                'italian'   : 'it',
-                'russian'   : 'ru',
-                'spanish'   : 'es',
-                'polish'    : 'pl',
-                'waray-waray' : 'war',
-                'cebuano'   : 'ceb',
-                'cietnamese': 'vi',
-                'japanese'  : 'ja',
-                'portuguese': 'pt',
-                'chinese'   : 'zh',
-                'ukranian'  : 'uk',
-                'catalan'   : 'ca',
-                'norwegian' : 'no',
-                'finnish'   : 'fi',
-                'persian'   : 'fa',
-                'indonesian': 'id',
-                'czech'     : 'cs',
-                'korean'    : 'ko',
-                'hungarian' : 'hu',
-                'arabic'    : 'ar',
-                'malay'     : 'ms',
-                'romanian'  : 'ro',
-                'serbian'   : 'sr',
-                'minangkabau':'min',
-                'turkish'   : 'tr',
-                'kazakh'    : 'kk',
-                'slovak'    : 'sk',
-                'esperanto' : 'eo',
-                'danish'    : 'da',
-                'basque'    : 'eu',
-                'lithuanian': 'lt',
-                'bulgarian' : 'bg',
-                'hebrew'    : 'he',
-                'croatian'  : 'hr',
-                'slovenian' : 'sl',
-                'uzbek'     : 'uz',
-                'volapuk'   : 'vo',
-                'estonian'  : 'et',
-                'hindi'     : 'hi',
-                'galician'  : 'gl',
-                'nynorsk'   : 'nn',
-                'simple'    : 'simple'
-            }[language.lower()]
-        except:
-            return language
-            
-            
-
+                    statistics.write(json.dumps(self.stats)+'\n')
+                    
 if __name__ == "__main__":
-    main()
+   bot = WikiBot()
+   bot.setUp()
+   bot.run()
     
